6467c2b3f33287c1834c641b66e96ebdd44a4706
Alemji <Alemji@users.noreply.github.com>
2015-08-19 16:46:52 -0400

    20150819 all components added

--- js/app.js
+++ js/app.js
@@ -5,35 +5,41 @@ $(document).ready(function(){
 		$("input[type='text']").val() + "</li>");
 		//appends text input to the list
 
-		})
+	})
 
-$('ul').on('click', 'li', function(event){ 
-		$(this).css('color', '#5d5d77');
-		$(this).css('text-decoration', 'line-through');
-		//changes color and adds line through checked items
+	$('ul').on('click', 'input', function(event){ 
+		if ($(this).prop('checked')) {
+			$(this).parent().css('color', '#5d5d77');
+			$(this).parent().css('text-decoration', 'line-through');	
+		}
+		 else {
+		 	$(this).parent().css('color', '');
+		 	$(this).parent().css('text-decoration', 'none');
+		 	//changes color and adds line through checked items
+		 }
+		
+		
 
-		 })
-
-
-$('.edit').click(function(){
-	if(confirm("delete checked items?")){
-		$("input:checkbox:checked").remove();
-
-		//else{return false;}
-	}
-
-		})
+	 })
 
 
+	$('.edit').click(function(){
+		if(confirm("Delete checked items?")){
+			$('input:checked').parent().remove();
+		}
 
-//why do I have to re-open the page to get it to work?
+	})
+
+	$('.clear').click(function(){
+		if(confirm("Clear entire list?")){
+			$('ul').empty();
+			//clears list
+		}
+
+	})
 
 
 
-
-//edit button
-
-//clear list button
 
 
 });
